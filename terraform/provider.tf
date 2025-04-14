9471731dcd63f58416b029d17ed86f8cb4d4d511
ramy wageh <ramywageh1996@gmail.com>
2025-04-15 01:31:38 +0200

    terraform inti 2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-2"
-  shared_config_files      = ["/root/.aws/config"]
-  shared_credentials_files = ["/root/.aws/credentials"]
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
