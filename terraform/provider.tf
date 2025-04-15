4cd141d20d430291157afd39d179091c5bf2fd80
ramy wageh <ramywageh1996@gmail.com>
2025-04-15 10:16:39 +0200

    terraform and Jenkins

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = "us-east-2"
+  shared_config_files      = ["/root/.aws/config"]
+  shared_credentials_files = ["/root/.aws/credentials"]
 }
