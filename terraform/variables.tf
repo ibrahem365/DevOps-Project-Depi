315217e64c0858344d2e2eb315256082056aed73
ramy wageh <ramywageh1996@gmail.com>
2025-04-15 02:58:18 +0200

    terraform inti 4

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,7 +20,7 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-396913711485"
 }
 
 variable "instance_type" {
