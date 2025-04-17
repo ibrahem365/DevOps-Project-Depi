87be32d4aba2a0ba10acf26eb0928ae8c1211d4f
Aya Esmail <76392909+ayaAhmedEsmail@users.noreply.github.com>
2025-04-17 11:09:02 +0200

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -5,9 +5,12 @@ resource "aws_instance" "instance" {
   
   key_name = var.key-name
   
-  user_data = <<-EOF
+   user_data = <<-EOF
               #!/bin/bash
+              mkdir -p /home/ubuntu/.ssh
               echo "${var.ssh_key_content}" >> /home/ubuntu/.ssh/authorized_keys
+              chmod 600 /home/ubuntu/.ssh/authorized_keys
+              chown -R ubuntu:ubuntu /home/ubuntu/.ssh
               EOF
 
   tags = {
