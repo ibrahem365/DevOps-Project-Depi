6a2736f3393a5dbd8f2193197bbc5e1f5fbac8a4
Aya Esmail <76392909+ayaAhmedEsmail@users.noreply.github.com>
2025-04-17 10:28:20 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "todo-app" {
   source = "./modules/ec2"
   ami           = "ami-084568db4383264d4"
   sg-id  = [module.todo-app-sg.id]
-  ssh_key_path = file(var.ssh_key_path)
+ ssh_key = var.ssh_key_content
   key-name = "connection"
   name = "todo-app"
 }
